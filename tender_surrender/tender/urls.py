6bea7a920d0acc35895222df3f48221b2b98d44b
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-03-23 12:36:48 +0530

    fix: Login fixed

--- tender_surrender/tender/urls.py
+++ tender_surrender/tender/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('add-user/<type_of_user>/', views.add_user, name="add-user"),
     path('add-project/', views.add_project, name="add-project"),
     path('add-task/<project_id>/', views.add_task, name="add-task"),
+    path('login/', views.login, name="login")
     path('tender-request/', views.tender_request, name="tender-request"),
     path('request-tender/', views.tender_request, name="request-tender"),
 ]
